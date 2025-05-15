996f18d7a48e8b17b18e1b59ca1d67afc91126bc
dev-aglailson <devaglailson@gmail.com>
2025-05-15 18:43:22 -0300

    Ajustes tema, ja comformulario de lead para partner mais sessão de redes

---
+++ resources/js/form-lead-partner.js
@@ -0,0 +1,66 @@
+window.addEventListener('load', function () {
+
+  let btnCadastroLead = document.getElementById('btn-cadastro-lead-partner')
+
+  btnCadastroLead.addEventListener('click', async (event) => {
+
+    event.preventDefault();
+
+    let formLead = document.getElementById('form-cadastro-lead');
+    
+    try {
+      
+      let dados   = await fieldsValidator(formLead)
+
+      preloader.show('Enviando dados...');
+
+      await sendData(dados)
+
+      preloader.hide()
+      
+      formLead.reset()
+      
+    } catch (error) {
+      
+      showMessage('error', error.message, 1000)
+      preloader.hide()
+    }
+  });
+
+
+  function fieldsValidator(formLead) {
+    return new Promise((resolve, reject) => { // <-- Adicionado o return aqui
+      let dados = {
+        nome: formLead['ipt-nome'].value,
+        email: formLead['ipt-email'].value,
+        celular: formLead['ipt-celular'].value,
+        termoPrivacidade: formLead['ipt-termos-politica-privacidade'].checked,
+      };
+
+      for (const [chave, valor] of Object.entries(dados)) {
+        if (!valor) {
+
+          let message = `Campo "${chave}" é obrigatório.`;
+
+          switch (chave) {
+            case "termoPrivacidade": message = 'É necessário aceitar os termos de privacidade!'; break;
+          }
+
+          return reject(new Error(message));
+        }
+      }
+
+      resolve(dados);
+    });
+  }
+
+  function sendData(dados){
+    return new Promise((resolve, reject) => {
+      showMessage('success', 'Dados enviado com sucesso!')
+      setTimeout(() => {
+        resolve()
+      }, 2000);
+    })
+  }
+
+})
--- resources/js/mascaras.js
+++ resources/js/mascaras.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
-    /* MASCARA CEP */
-    $('.cep').mask('00000-000');
-   
-    
+$(document).ready(function () {
+  /* MASCARA CEP */
+  $('.cep').mask('00000-000');
+  
+  $('.mask-celular').mask('(00) 00000-0000')
+
 })
